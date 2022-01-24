1ba047fc758fb87d3a9ade5660d89f61e2255105
sTefbured <stef.for.ever2000@gmail.com>
2022-01-25 01:57:38 +0300

    Initial commit.

---
+++ on-call-server/src/main/java/com/stefbured/oncallserver/OnCallServerApplication.java
@@ -0,0 +1,13 @@
+package com.stefbured.oncallserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnCallServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnCallServerApplication.class, args);
+    }
+
+}
